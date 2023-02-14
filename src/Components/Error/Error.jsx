3261b58dd170085163e6941f90218c3a29cbe5ee
Md Rifat Hossen <rifatsaown0@gmail.com>
2023-02-15 02:24:04 +0600

    react-router explore : error page design

---
+++ src/Components/Error/Error.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Error = () => {
+  return (
+    <div>
+      <h1>404 Error : No Page Found On This Link .</h1>
+      <h2>Or</h2>
+      <h1>Invalid Link</h1>
+    </div>
+  );
+};
+
+export default Error;

8fd730fd53d2a768fd5fa75eeeab21877e9b0627
Md Rifat Hossen <rifatsaown0@gmail.com>
2023-02-19 04:05:55 +0600

    yarn remove

--- src/Components/Friends/Friends.jsx
+++ src/Components/Friends/Friends.jsx
@@ -1,17 +1,24 @@
-import React from 'react';
-import { useLoaderData } from 'react-router-dom';
-import Friend from '../../Friend/Friend';
-import './Friends.css';
+import React from "react";
+import { useLoaderData } from "react-router-dom";
+import Friend from "../../Friend/Friend";
+import "./Friends.css";
 
 const Friends = () => {
-    const friends = useLoaderData();
-    return (
-        <div className='friends-data'>
-            {
-                friends.map((friend,index) => <Friend key={index} friend={friend}/>)
-            }
-        </div>
-    );
+  const friends = useLoaderData();
+  return (
+    <div>
+        <h1>Friends : {friends.length}</h1>
+      <div className="friends-data">
+        {friends.length ? (
+          friends.map((friend) => (
+            <Friend friend={friend} key={friend.id}></Friend>
+          ))
+        ) : (
+          <h1>Loading...</h1>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Friends;
